b3e86513357413a5ad58684d3dca28d7ff755396
727021 <1834544+727021@users.noreply.github.com>
2025-09-08 23:09:27 -0600

    set up nextjs and clerk

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,11 @@
 import js from '@eslint/js'
 import prettier from 'eslint-config-prettier'
 import globals from 'globals'
+import { FlatCompat } from '@eslint/eslintrc'
+
+const compat = new FlatCompat({
+  baseDirectory: import.meta.dirname
+})
 
 /**
  * @type {import('eslint').Linter.Config[]}
@@ -12,6 +17,23 @@ export default [
       globals: {
         ...globals.node
       }
+    }
+  },
+  ...compat.config({
+    extends: ['next'],
+    settings: {
+      next: {
+        rootDor: 'web/'
+      }
+    },
+    rules: {
+      '@next/next/no-html-link-for-pages': 'off'
+    }
+  }),
+  {
+    files: ['./*'],
+    rules: {
+      'import/no-anonymous-default-export': 'off'
     }
   },
   prettier
